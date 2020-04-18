11655d7330710d5c788fbc1eab76888c8f3351c5
Zamir <baizakoff2017@gmail.com>
2020-04-18 13:58:24 +0300

    button

--- script/script.js
+++ script/script.js
@@ -17,12 +17,12 @@ $('.screen').width(width);
 $('.screen').height(height);
 
 
-$('.screen ul li').width(width);
-$('.screen ul li').height(height);
+$('.screen .slides li').width(width);
+$('.screen .slides li').height(height);
 
 let slideCnt = $('.screen ul li').length;
 
-$('.screen ul').width(width * slideCnt);
+$('.screen .slides').width(width * slideCnt);
 
 //////////////
 
@@ -41,28 +41,88 @@ $('.screen ul').width(width * slideCnt);
 
 
 let page = 0;
-let speed = 500;
+const SPEED = 500;
 
 function goRight() {
     if (page === slideCnt - 1) {
         return
     }
-    $('.screen ul').animate({ 'left': `-${++page * width}px` }, speed);
+    $('.screen .slides').animate({ 'left': `-${++page * width}px` }, SPEED);
+    changeActivePoints()
 }
 
 function goLeft() {
     if (page === 0) {
         return
     }
-    $('.screen ul').animate({ 'left': `-${--page * width}px` }, speed);
+    $('.screen .slides').animate({ 'left': `-${--page * width}px` }, SPEED);
+    changeActivePoints()
+}
+
+function changeActivePoints(){
+    $('.points li').removeClass('active');
+    $('.points li').eq(page).addClass('active');
 }
 
 
 
-
 $('.t-left').on('click', goLeft);
-
-
 $('.t-right').on('click', goRight);
 
+$('.points li').on('click', function(){
+    let cur = $(this);
 
+    $('.points li').removeClass('active')
+    cur.addClass('active');
+
+    let index = $('.points li').index(cur);
+    page = index;
+    $('.screen .slides').animate({ 'left': `-${index* width}px` }, SPEED);
+});
+
+// $('.points li').addClass('active')
+//     $(this).css({
+//         backgroundColor: "yellow"
+//     });
+
+
+// $('.points li').removeClass('active')
+//     $(this).css({
+
+//     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////
+    // $( "#btn1" ).click(function() {
+    //     function complete() {
+    //       $( "<div>" ).text( this.id ).appendTo( "#log" );
+    //     }
+    //     $( "#box1" ).fadeOut( 1600, "linear", complete );
+    //     $( "#box2" ).fadeOut( 1600, complete );
+    //   });
+       
+    //   $( "#btn2" ).click(function() {
+    //     $( "div" ).show();
+    //     $( "#log" ).empty();
+    //   });
